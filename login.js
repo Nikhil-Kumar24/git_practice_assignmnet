0c3053a8183b9245b2093c19b89758b18ee4c9fc
nikhil kumar <nikhil24e927@gmail.com>
2022-12-01 23:43:44 +0530

    login_reg

---
+++ login.js
@@ -0,0 +1,30 @@
+// PRIME NUMBER
+
+let count = 0;
+for(let i = 1; i<num; i++)
+{
+if(num % i ==0)
+{
+count+
+}
+}
+if(count == )
+{
+console.log("yes prime")
+}
+else{
+
+
+
+//PALINDROME
+
+let new_str = "";
+for(let i = N; i >=0; i--)
+{
+new_str = new_str + str[i]
+}
+if(str == new_str)
+{
+console.log("yes string is palindrome");
+}
+
